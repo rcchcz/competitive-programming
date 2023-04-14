5d6238f31771a45d81dadf8ab6df42d7b0ab0cac
Rita Cruz <rcchcz@gmail.com>
2023-04-14 11:27:15 -0300

    add problem #516

---
+++ longest_palindromic_subsequence.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/longest-palindromic-subsequence/
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        return self.longestCommonSubsequence(s, s[::-1])
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+
+        for i in range(len(text1)-1, -1, -1):
+            for j in range(len(text2)-1, -1, -1):
+                if text1[i] == text2[j]: dp[i][j] = 1 + dp[i+1][j+1]
+                else: dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+        
+        return dp[0][0]
