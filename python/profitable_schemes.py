988047b7d87d71c3d3944ff98d66f716ed0db7d7
Rita Cruz <rcchcz@gmail.com>
2023-04-21 16:47:37 -0300

    add problem #879

---
+++ python/profitable_schemes.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/profitable-schemes/
+class Solution:
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        MOD = 10**9 + 7
+        dp = defaultdict(int) # (i, m, p) -> num of ways
+
+        for m in range(n + 1):
+            dp[(len(group), m, minProfit)] = 1
+
+        for i in range(len(group) - 1, -1, -1):
+            for m in range(n + 1):
+                for p in range(minProfit + 1):
+                    dp[(i, m, p)] = dp[(i + 1, m, p)]
+                    if m + group[i] <= n:
+                        dp[(i, m, p)] += dp[(i + 1, m + group[i], min(minProfit, p + profit[i]))] % MOD
+        
+        return dp[(0, 0, 0)] % MOD
