5e43e62bc4c9dc7ed0746938790056cd33a0938a
Rita Cruz <rcchcz@gmail.com>
2023-05-03 08:57:23 -0300

    add problem #905

---
+++ python/sort_array_by_parity.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/sort-array-by-parity/
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        nums.sort(key=lambda x: x%2)
+
+        return nums
